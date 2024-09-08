e8ca2a2424a363cff0f2b0e246591bc640ca41ab
yuta-fukui-823290 <yuta.fukui_823290@glv.co.jp>
2024-09-08 16:27:56 +0900

    envの型定義ファイルの追加

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,7 +3,6 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
-
 declare namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_SUPABASE_URL: string;
